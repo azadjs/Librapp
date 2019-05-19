cde18c5d3a6001f78651899941e33a72c9d27ad7
Azad M <azad.mamiyev.98@gmail.com>
2019-05-19 18:21:40 +0400

    Small Changes 2

--- app/src/main/java/com/azadjs/librapp/Parse.java
+++ app/src/main/java/com/azadjs/librapp/Parse.java
@@ -46,22 +46,6 @@ public class Parse extends AsyncTask<Void,Void,Void> {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            try {
-                Document document = Jsoup.connect(InstantAddActivity.appUrl.getText().toString()).get();
-                Element eNameElement = document.select("h1.AHFaub>span").first();
-                String eName = eNameElement.text();
-                seteName(eName);
-                System.out.println(eName);
-
-                Elements eImageElement = document.select("div.xSyT2c img[src]");
-                String eImage = eImageElement.attr("src");
-                seteImage(eImage);
-                System.out.println(eImage);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         return null;
     }
 
